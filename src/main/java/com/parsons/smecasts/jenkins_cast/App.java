120347f34b037599a42c4fe28ff180c4b2d90ade
akhan <adnan.khan@parsons.com>
2016-10-12 15:04:07 -0400

    init commit

---
+++ src/main/java/com/parsons/smecasts/jenkins_cast/App.java
@@ -0,0 +1,18 @@
+package com.parsons.smecasts.jenkins_cast;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	String html = Jsoup.connect("example.com").get().html();
+    	System.out.println(html);
+    }
+}
